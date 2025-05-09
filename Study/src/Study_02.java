1cd5ba0c2a74560c4038e8ab1ef51793fbcc01f2
JANGSAEHYEON <rokuel6l@metaskyworld.net>
2025-05-09 17:50:07 +0900

    공부

--- Study/src/Study_02.java
+++ Study/src/Study_02.java
@@ -2,6 +2,15 @@ public class Study_02
 {
     public static void main(String[] args)
     {
+        Test t = new Test();
+        Test t1 = new Test();
 
+        System.out.println(t.ReturnTest());
+        Test.count++;
+        System.out.println(t.ReturnTest());
+        System.out.println(t1.ReturnTest());
+        Test.count++;
+        System.out.println(t.ReturnTest());
+        System.out.println(t1.ReturnTest());
     }
 }
---
+++ Study/src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static int count;
+
+    public int ReturnTest(){
+        return count;
+    }
+}
